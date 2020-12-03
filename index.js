fb59eeffa98f602a36c031c6a1b67608285e3cd2
alex-rey74 <56680869+alex-rey74@users.noreply.github.com>
2020-12-03 23:15:27 +0100

    Test trigger

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('test');
+export default async function (context, req) {
+    context.log('Trigger function');
+
+    const responseMessage = "This HTTP triggered function executed successfuly";
+
+    context.res = {
+        body: responseMessage
+    };
+}
