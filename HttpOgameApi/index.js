5c591bb95430e02a1847460acdbb84a408a8b734
alex-rey74 <56680869+alex-rey74@users.noreply.github.com>
2020-12-03 23:33:25 +0100

    Get player

--- HttpOgameApi/index.js
+++ HttpOgameApi/index.js
@@ -1,13 +1,22 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const xhr = new XMLHttpRequest();
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    xhr.onload = function(){
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: xhr.responseXML.documentElement.nodeName
+        };
+    }
+
+    xhr.onerror = function(){
+        context.res = {
+            status: 500
+        };
+    }
+
+    xhr.open("GET", "https://s167-fr.ogame.gameforge.com/api/players.xml");
+    xhr.responseType = "document";
+    xhr.send();
 }
